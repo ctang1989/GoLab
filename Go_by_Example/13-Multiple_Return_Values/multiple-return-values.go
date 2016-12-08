6d5c3aef41614d16ab3a702e6f6bd71269b3b260
ctang1989 <mchina_tang@qq.com>
2016-12-08 21:03:11 +0800

    Add 10-17

---
+++ Go_by_Example/13-Multiple_Return_Values/multiple-return-values.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func vals() (int, int) {
+	return 3, 7
+}
+
+func main() {
+
+	a, b := vals()
+	fmt.Println(a)
+	fmt.Println(b)
+
+	_, c := vals()
+	fmt.Println(c)
+}
+
+
+/*
+$ go run multiple-return-values.go 
+3
+7
+7
+*/
+
+// https://gobyexample.com/multiple-return-values
