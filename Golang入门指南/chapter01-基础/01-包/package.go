13c926a628c789bdb9baba231076bc62ff40c713
ctang1989 <mchina_tang@qq.com>
2017-03-08 22:41:05 +0800

    Golang入门指南

---
+++ Golang入门指南/chapter01-基础/01-包/package.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("My favorite number is", rand.Intn(10))
+}
+
+// https://yushuangqi.com/book/gotour/basics/package.html
---
+++ Golang入门指南/chapter01-基础/02-导入/import.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"hash/adler32"
+	"os"
+
+	"appengine/foo"
+	"appengine/user"
+
+	"code.google.com/p/x/y"
+	"github.com/ysqi/beego"
+)
+
+// https://yushuangqi.com/book/gotour/basics/import.html
